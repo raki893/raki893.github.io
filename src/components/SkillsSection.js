99c63916b938ffc7df3bb5dc874f2665fd58e569
Raki893 <Raki893@gmail.com>
2023-10-10 19:12:09 +0200

    tunned navbar

--- src/components/SkillsSection.js
+++ src/components/SkillsSection.js
@@ -4,66 +4,7 @@ const SkillsSection = () => {
   return (
     <section>
       <h2>Skills</h2>
-      <div className="skills">
-
-        <div className="charts">
-          <div className="chart chart--dev">
-            <span className="chart__title">Development</span>
-            <ul className="chart--horiz">
-              <li className="chart__bar" style={{ width: '90%' }}>
-                <span className="chart__label">
-                  React
-                </span>
-              </li>
-              <li className="chart__bar" style={{ width: '98%' }}>
-                <span className="chart__label">
-                  HTML & CSS 
-                </span>
-              </li>
-              <li className="chart__bar" style={{ width: '95%' }}>
-                <span className="chart__label">
-                  JavaScript
-                </span>
-              </li>
-              {/* Add other development-related chart bars here */}
-            </ul>
-          </div>
-
-          <div className="chart chart--prod">
-            <span className="chart__title">Productivity</span>
-            <ul className="chart--horiz">
-              <li className="chart__bar" style={{ width: '75%' }}>
-                <span className="chart__label">
-                  Git + Github
-                </span>
-              </li>
-              <li className="chart__bar" style={{ width: '80%' }}>
-                <span className="chart__label">
-                  Microsoft Office
-                </span>
-              </li>
-              {/* Add other productivity-related chart bars here */}
-            </ul>
-          </div>
-
-          <div className="chart chart--design">
-            <span className="chart__title">Design</span>
-            <ul className="chart--horiz">
-              <li className="chart__bar" style={{ width: '80%' }}>
-                <span className="chart__label">
-                  Figma
-                </span>
-              </li>
-              <li className="chart__bar" style={{ width: '80%' }}>
-                <span className="chart__label">
-                  Photoshop
-                </span>
-              </li>
-              {/* Add other design-related chart bars here */}
-            </ul>
-          </div>
-        </div>
-      </div>
+      
 
     </section>
   );
