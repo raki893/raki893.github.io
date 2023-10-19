0960d03347100e067c37584ae51c5438daec7bc9
Raki893 <Raki893@gmail.com>
2023-10-19 12:16:45 +0200

    finished footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,80 +4,22 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="background">
-        <svg className="footer_svg"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-          x="0px"
-          y="0px"
-          width="100%"
-          height="100%"
-          viewBox="0 0 1600 900"
-        >
-          <defs>
-            <linearGradient id="bg" x2="0%" y2="100%">
-              <stop offset="0%" style={{ stopColor: 'rgba(53, 127, 242, 0.6)' }}></stop>
-              <stop offset="100%" style={{ stopColor: 'rgba(38, 89, 190, 0.06)' }}></stop>
-            </linearGradient>
-            <path
-              id="wave"
-              fill="url(#bg)"
-              d="M-363.852,502.589c0,0,236.988-41.997,505.475,0
-      s371.981,38.998,575.971,0s293.985-39.278,505.474,5.859s493.475,48.368,716.963-4.995v560.106H-363.852V502.589z"
-            />
-          </defs>
-          <g>
-            <use xlinkHref="#wave" opacity=".3">
-              <animateTransform
-                attributeName="transform"
-                attributeType="XML"
-                type="translate"
-                dur="8s"
-                calcMode="spline"
-                values="270 230; -334 180; 270 230"
-                keyTimes="0; .5; 1"
-                keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
-                repeatCount="indefinite"
-              />
-            </use>
-            <use xlinkHref="#wave" opacity=".6">
-              <animateTransform
-                attributeName="transform"
-                attributeType="XML"
-                type="translate"
-                dur="6s"
-                calcMode="spline"
-                values="-270 230;243 220;-270 230"
-                keyTimes="0; .6; 1"
-                keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
-                repeatCount="indefinite"
-              />
-            </use>
-            <use xlinkHref="#wave" opacity=".9">
-              <animateTransform
-                attributeName="transform"
-                attributeType="XML"
-                type="translate"
-                dur="4s"
-                calcMode="spline"
-                values="0 230;-140 200;0 230"
-                keyTimes="0; .4; 1"
-                keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
-                repeatCount="indefinite"
-              />
-            </use>
-          </g>
-        </svg>
-      </div>
-
-      <section className='social_section'>
-        <ul className="socials">
-          
-        </ul>
-
-        <p className="legal">© Md Rakibul Islam - 2023 All rights reserved</p>
-      </section>
+      <p className="legal">© Md Rakibul Islam - 2023 All rights reserved</p>
+      <div className="wave">
+            <svg className="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+                viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
+                <defs>
+                    <path id="gentle-wave"
+                        d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+                </defs>
+                <g className="parallax">
+                    <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(0,0,150,0.7)" />
+                    <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(0,0,150,0.5)" />
+                    <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(0,0,150,0.3)" />
+                    <use xlinkHref="#gentle-wave" x="48" y="7" fill="rgba(0,0,150)" />
+                </g>
+            </svg>
+        </div>
     </footer>
   );
 };
