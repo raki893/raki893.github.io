116c6404a02da97f4b4438941148836f9396a545
Raki893 <Raki893@gmail.com>
2023-10-23 23:39:08 +0200

    unfinished project section

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,13 +1,43 @@
 import React from 'react';
 
+const ProjectCard = ({ title, description, buttonLabel }) => {
+  return (
+    <div className="card">
+      <div className="content">
+        <h2 className="title">{title}</h2>
+        <p className="copy">{description}</p>
+        <button className="btn">{buttonLabel}</button>
+      </div>
+    </div>
+  );
+};
+
 const ProjectsSection = () => {
   return (
-    <div className='projects'>
+    <div className="projects">
       <h2>Projects</h2>
-      
-      
-      
-
+      <main className="page-content">
+        <ProjectCard
+          title="Mountain View"
+          description="Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains"
+          buttonLabel="View Trips"
+        />
+        <ProjectCard
+          title="To The Beach"
+          description="Plan your next beach trip with these fabulous destinations"
+          buttonLabel="View Trips"
+        />
+        <ProjectCard
+          title="Desert Destinations"
+          description="It's the desert you've always dreamed of"
+          buttonLabel="Book Now"
+        />
+        <ProjectCard
+          title="Explore The Galaxy"
+          description="Seriously, straight up, just blast off into outer space today"
+          buttonLabel="Book Now"
+        />
+      </main>
     </div>
   );
 };
