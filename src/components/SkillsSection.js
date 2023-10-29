c9b517a73bc950be7adf7930ca2d47bc4f449191
Raki893 <Raki893@gmail.com>
2023-10-29 21:15:34 +0100

    fixed github logo

--- src/components/SkillsSection.js
+++ src/components/SkillsSection.js
@@ -228,9 +228,8 @@ const SkillsSection = () => {
         <div className="svg_item">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width={120}
-            height={120}
-            viewBox="0 0 20 20"
+            
+            viewBox="0 0 22 22"
 
           >
             <title>{"github [#142]"}</title>
